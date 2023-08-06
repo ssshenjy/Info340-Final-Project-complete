bb1f77ee7a9fa45d36a604674596d3b8e663b667
candiiiceH <zh365@uw.edu>
2023-08-05 21:32:30 -0700

    re-setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import {BrowserRouter} from 'react-router-dom';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'; //import css file!
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BrowserRouter><App /></BrowserRouter>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
