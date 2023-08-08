20eb2470cfbdc83cc5a7bcdc1e264483dac94c22
lichuanwang <wanglichuan127@gmail.com>
2023-08-08 21:12:00 +0800

    fixing introduction and addevent

--- src/AddEvent.js
+++ src/AddEvent.js
@@ -1,56 +1,68 @@
 import React from 'react';
 
+// Form Input Component
+function FormInput({ id, name, type = "text", label, required = false }) {
+  return (
+    <div className="mb-3">
+      <label htmlFor={id} className="form-label">{label}</label>
+      <input type={type} className="form-control" id={id} name={name} required={required} />
+    </div>
+  );
+}
+
+// Form Textarea Component
+function FormTextarea({ id, name, label }) {
+  return (
+    <div className="mb-3">
+      <label htmlFor={id} className="form-label">{label}</label>
+      <textarea className="form-control" id={id} name={name}></textarea>
+    </div>
+  );
+}
+
+// Form Checkbox Component
+function FormCheckbox({ id, name, label, required = false }) {
+  return (
+    <div className="mb-3 form-check">
+      <input type="checkbox" className="form-check-input" id={id} name={name} required={required} />
+      <label className="form-check-label" htmlFor={id}>{label}</label>
+    </div>
+  );
+}
+
+// Footer Component
+function Footer() {
+  return (
+    <footer className="bg-light py-3 mt-5">
+      <div className="container text-center">
+        <p>&copy; 2023 Journemo. All rights reserved.</p>
+        <p id="contact">
+          Contact Us: <a href="mailto:info@journemo.com"><span className="material-icons"></span>info@journemo.com</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+// Main AddEventForm Component
 function AddEventForm() {
   return (
     <div>
-        <main className="container mt-4">
+      <main className="container mt-4">
         <form>
-            <div className="mb-3">
-            <label htmlFor="eventName" className="form-label">Event Name:</label>
-            <input type="text" className="form-control" id="eventName" name="eventName" required />
-            </div>
-
-            <div className="mb-3">
-            <label htmlFor="eventDate" className="form-label">Event Date:</label>
-            <input type="date" className="form-control" id="eventDate" name="eventDate" required />
-            </div>
-
-            <div className="mb-3">
-            <label htmlFor="eventTime" className="form-label">Event Time:</label>
-            <input type="time" className="form-control" id="eventTime" name="eventTime" />
-            </div>
-
-            <div className="mb-3">
-            <label htmlFor="location" className="form-label">Location:</label>
-            <input type="text" className="form-control" id="location" name="location" />
-            </div>
-
-            <div className="mb-3">
-            <label htmlFor="description" className="form-label">Description:</label>
-            <textarea className="form-control" id="description" name="description"></textarea>
-            </div>
-
-            <div className="mb-3 form-check">
-            <input type="checkbox" className="form-check-input" id="itinerary" name="itinerary" required />
-            <label className="form-check-label" htmlFor="itinerary">Is It An Itinerary?</label>
-            </div>
-
-            <button type="submit" className="btn btn-primary">Add Event</button>
+          <FormInput id="eventName" name="eventName" label="Event Name:" required />
+          <FormInput id="eventDate" name="eventDate" type="date" label="Event Date:" required />
+          <FormInput id="eventTime" name="eventTime" type="time" label="Event Time:" />
+          <FormInput id="location" name="location" label="Location:" />
+          <FormTextarea id="description" name="description" label="Description:" />
+          <FormCheckbox id="itinerary" name="itinerary" label="Is It An Itinerary?" required />
+          <button type="submit" className="btn btn-primary">Add Event</button>
         </form>
-        </main>
-
-        <footer className="bg-light py-3 mt-5">
-            <div className="container text-center">
-            <p>&copy; 2023 Journemo. All rights reserved.</p>
-            <p id="contact">
-                Contact Us: <a href="mailto:info@journemo.com"><span className="material-icons"></span>info@journemo.com</a>
-            </p>
-            </div>
-        </footer>
+      </main>
+      <Footer />
     </div>
   );
 }
 
 export default AddEventForm;
-
 
--- src/Introduction.js
+++ src/Introduction.js
@@ -4,73 +4,87 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// Hero Section
+function HeroSection() {
+  return (
+    <header className="hero-image">
+      <h2 className="display-4 text-shadow">The easiest way to plan your travels</h2>
+      <p className="lead text-shadow">All your travel plans, all in one place.</p>
+      <Link to="/input" className="btn btn-outline-light btn-lg">Get started for free</Link>
+    </header>
+  );
+}
+
+// Individual Feature
+function Feature({ title, description }) {
+  return (
+    <div className="col-md-4">
+      <div className="card mb-4">
+        <div className="card-body">
+          <h4 className="card-title">{title}</h4>
+          <p className="card-text">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Features Section
+function FeaturesSection() {
+  return (
+    <section className="features-section" id="features">
+      <h3 className="text-center mb-5">Why Journemo?</h3>
+      <div className="row">
+        <Feature 
+          title="Interactive Itinerary" 
+          description="Create, manage and visualize your itinerary in a dynamic calendar view. Cover all the important details for each day of your travel." 
+        />
+        <Feature 
+          title="Add Events and Notes" 
+          description="Add important events and notes to your itinerary. Attach contact information, addresses and other relevant data to each event." 
+        />
+        <Feature 
+          title="Budget Tracking" 
+          description="Set your travel budget and enter the estimated costs for each event in your schedule. Keep track of your expenses and make sure you stay within your budget." 
+        />
+      </div>
+    </section>
+  );
+}
+
+// Call to Action
+function CallToAction() {
+  return (
+    <section className="row mt-5">
+      <div className="col-lg-12 text-center">
+        <h2>Ready to start your adventure?</h2>
+        <a href="#signup" className="btn btn-primary btn-lg">Sign Up for free</a>
+      </div>
+    </section>
+  );
+}
+
+// Footer
+function Footer() {
+  return (
+    <footer className="bg-light py-3 mt-5">
+      <div className="container text-center">
+        <p>&copy; 2023 Journemo. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+// Main Introduction Component
 function Introduction(props) {
   return (
     <div>
-      {/* Navigation bar */}
-
-
-      {/* Hero section */}
-      <header className="hero-image">
-        <h2 className="display-4 text-shadow">The easiest way to plan your travels</h2> {/* Changed from h1 to h2 */}
-        <p className="lead text-shadow">All your travel plans, all in one place.</p> {/* Add shadow to the text */}
-        <Link to="/input" className="btn btn-outline-light btn-lg">Get started for free</Link>
-      </header>
-
-      {/* Main Content */}
+      <HeroSection />
       <main className="container mt-4">
-        {/* Features Section */}
-        <section className="features-section" id="features">
-          <h3 className="text-center mb-5">Why Journemo?</h3> {/* Changed from h2 to h3 */}
-          <div className="row">
-            {/* Feature 1 */}
-            <div className="col-md-4">
-              <div className="card mb-4">
-                <div className="card-body">
-                  <h4 className="card-title">Interactive Itinerary</h4> {/* Changed from h5 to h4 */}
-                  <p className="card-text">Create, manage and visualize your itinerary in a dynamic calendar view. Cover all the important details for each day of your travel.</p>
-                </div>
-              </div>
-            </div>
-            {/* Feature 2 */}
-            <div className="col-md-4">
-              <div className="card mb-4">
-                <div className="card-body">
-                  <h5 className="card-title">Add Events and Notes</h5>
-                  <p className="card-text">Add important events and notes to your itinerary. Attach contact information, addresses and other relevant data to each event.</p>
-                </div>
-              </div>
-            </div>
-            {/* Feature 3 */}
-            <div className="col-md-4">
-              <div className="card mb-4">
-                <div className="card-body">
-                  <h5 className="card-title">Budget Tracking</h5>
-                  <p className="card-text">Set your travel budget and enter the estimated costs for each event in your schedule. Keep track of your expenses and make sure you stay within your budget.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        {/* Call to action */}
-        <section className="row mt-5">
-          <div className="col-lg-12 text-center">
-            <h2>Ready to start your adventure?</h2>
-            <a href="#signup" className="btn btn-primary btn-lg">Sign Up for free</a>
-          </div>
-        </section>
+        <FeaturesSection />
+        <CallToAction />
       </main>
-
-      {/* Footer */}
-      <footer className="bg-light py-3 mt-5">
-        <div className="container text-center">
-          <p>&copy; 2023 Journemo. All rights reserved.</p>
-        </div>
-      </footer>
-
-      {/* Bootstrap Bundle with Popper */}
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-fnMzBIW/AVKdW1ZKh5vBvtO4jB1JZmp3hSYLs5QDfMhUu6IG5gZKpy4ij4b3R9dg" crossorigin="anonymous"></script>
+      <Footer />
     </div>
   );
 }
