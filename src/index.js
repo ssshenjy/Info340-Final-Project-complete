172d173586e85a5c23d8e4eb12cfca4e4b31f967
ssshenjy <ssshenjy@126.com>
2023-08-10 14:47:42 -0700

    index.html link and script removed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
