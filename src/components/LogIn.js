860ce42052f04ffbed0f96ae34a7adf921d4d3c7
lichuanwang <wanglichuan127@gmail.com>
2023-08-18 00:05:27 +0800

    login and sign up for authentication

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 function LogIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const auth = getAuth();
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("User logged in successfully!");
+
+      // Redirect to the introduction page after successful login
+      navigate('/introduction');
+      
+    } catch (error) {
+      console.error("Error logging in:", error.message);
+    }
+  };
+
   return (
     <div>
       <h2>Log In</h2>
-      <form>
-        <label>Email: <input type="email" name="email" /></label>
-        <label>Password: <input type="password" name="password" /></label>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input 
+            type="email" 
+            name="email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input 
+            type="password" 
+            name="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
         <button type="submit">Log In</button>
       </form>
     </div>
@@ -14,3 +53,7 @@ function LogIn() {
 }
 
 export default LogIn;
+
+
+
+
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getDatabase, ref, set as firebaseSet } from 'firebase/database';
+import { useNavigate } from 'react-router-dom';
 
 function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();  // Get the navigate function
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const auth = getAuth(); 
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      // Save the user data in Firebase Realtime Database
+      const db = getDatabase();
+      const userRef = ref(db, `users/${user.uid}`);
+      await firebaseSet(userRef, {
+        email: user.email
+      });
+
+      console.log("User registered and data saved successfully!");
+
+      // Redirect to the introduction page
+      navigate('/introduction');
+
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
+
   return (
     <div>
       <h2>Sign Up</h2>
-      <form>
-        <label>Email: <input type="email" name="email" /></label>
-        <label>Password: <input type="password" name="password" /></label>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input 
+            type="email" 
+            name="email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input 
+            type="password" 
+            name="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
         <button type="submit">Sign Up</button>
       </form>
     </div>
