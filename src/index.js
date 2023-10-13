16154127e4d7313660b526b77e6bce1c584894b8
ssshenjy <ssshenjy@126.com>
2023-10-12 20:35:10 -0700

    Before data references complete

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArj2ZGV7hCgVzayFHly0wvSk9gCd3VqU0",
+  authDomain: "info340-journemo-final.firebaseapp.com",
+  projectId: "info340-journemo-final",
+  storageBucket: "info340-journemo-final.appspot.com",
+  messagingSenderId: "872609713266",
+  appId: "1:872609713266:web:c7542461254acc55b586eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
